c86917a76d53c72dd731ed289347d0c608d05b8c
schand143 <shaikhchand724@gmail.com>
2020-02-05 20:09:05 +0530

    Updates

---
+++ precache-manifest.9070ebfc96dc102543e44f6b7cd13401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc527d88264d0a2c9e02907fd0dcdb3",
+    "url": "/schand143/todolist/index.html"
+  },
+  {
+    "revision": "211a1919205a0da06a9a",
+    "url": "/schand143/todolist/static/css/main.1e818318.chunk.css"
+  },
+  {
+    "revision": "1f1bb64c5fddfa1bd8f4",
+    "url": "/schand143/todolist/static/js/2.f8ba9ac8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/schand143/todolist/static/js/2.f8ba9ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211a1919205a0da06a9a",
+    "url": "/schand143/todolist/static/js/main.3946e32d.chunk.js"
+  },
+  {
+    "revision": "183236d50414508a344a",
+    "url": "/schand143/todolist/static/js/runtime-main.324ff652.js"
+  }
+]);
